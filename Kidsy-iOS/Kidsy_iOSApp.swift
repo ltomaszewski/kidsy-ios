e2e23ac2364c9203e0d76975537e9c7d181a4e45
Lucas Tomaszewski <luki22@gmail.com>
2024-04-27 20:40:02 +0200

    Initial Commit

---
+++ Kidsy-iOS/Kidsy_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kidsy_iOSApp.swift
+//  Kidsy-iOS
+//
+//  Created by Lukasz Tomaszewski on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kidsy_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
